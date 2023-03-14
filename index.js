9752adf406d396df4eaab1941d9b44e87dbd9a55
Romil <romil.m.kothari@gmail.com>
2023-03-14 16:48:22 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName () {
+        return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+     bestCustomer = `not ${customerName.toLowerCase()}`;
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = `maybe ${customerName.toLowerCase()}`
+}
+const leastFavoriteCustomer = "Romil";
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "Tommy"
+}
